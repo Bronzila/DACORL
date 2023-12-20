c77c4e0d839487d96cd06ce2da0f94f641a70b92
Janis Fix <janis_fix@web.de>
2023-12-20 13:21:14 +0100

    Add generate_data

--- src/utils/generate_data.py
+++ src/utils/generate_data.py
@@ -1,10 +1,16 @@
+import json
 import os
-import torch
-import numpy as np
 import random
-from utils.replay_buffer import ReplayBuffer
-from agents.step_decay import StepDecayAgent
 import signal
+
+import numpy as np
+import pandas as pd
+import torch
+from dacbench.benchmarks import ToySGDBenchmark
+
+from agents.step_decay import StepDecayAgent
+from utils.replay_buffer import ReplayBuffer
+
 
 # Time out related class and function
 class OutOfTimeException(Exception):
@@ -14,15 +20,41 @@ class OutOfTimeException(Exception):
 def timeouthandler(signum, frame):
     raise OutOfTimeException
 
+def set_seeds(seed):
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+def save_data(
+    save_run_data,
+    aggregated_run_data,
+    save_rep_buffer,
+    replay_buffer,
+    results_dir,
+    run_info,
+):
+    save_path = os.path.join(results_dir, f"rep_buffer")
+    if save_rep_buffer:
+        replay_buffer.save(save_path)
+        with open(os.path.join(results_dir, f"run_info.json"), "w") as f:
+            json.dump(run_info, f, indent=4)
+
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
+    if save_run_data:
+        aggregated_run_data.to_csv(os.path.join(results_dir, "aggregated_run_data.csv"))
+
 def get_environment(environment_type):
     if environment_type == "ToySGD":
         # setup benchmark
+        bench = ToySGDBenchmark()
+        return bench.get_environment()
     else:
         print(f"No environment of type {environment_type} found.")
 
-def generate_dataset(agent_type, agent_config, environment_type, function_name,
-                     num_runs, num_batches, seed, results_dir, reward_structure,
-                     state_version, save_run_data, save_rep_buffer, timeout):
+def generate_dataset(agent_type, agent_config, environment_type, num_runs,
+                     num_batches, seed, results_dir, save_run_data,
+                     save_rep_buffer, timeout):
 
     if timeout > 0:
         # conversion from hours to seconds
@@ -30,13 +62,15 @@ def generate_dataset(agent_type, agent_config, environment_type, function_name,
         signal.signal(signal.SIGALRM, timeouthandler)
         signal.alarm(timeout)
 
+    set_seeds(seed)
+
     if not (save_run_data or save_rep_buffer):
         input("You are not saving any results. Enter a key to continue anyway.")
 
     if results_dir == "":
-        results_dir = os.path.join("data", agent_type, function_name)
+        results_dir = os.path.join("data", agent_type, environment_type)
     else:
-        results_dir = os.path.join(results_dir, agent_type, function_name)
+        results_dir = os.path.join(results_dir, agent_type, environment_type)
 
     env = get_environment(environment_type)
     state = env.reset()[0]
@@ -51,110 +85,85 @@ def generate_dataset(agent_type, agent_config, environment_type, function_name,
         agent = StepDecayAgent(**agent_config)
     else:
         print(f"No agent with type {agent_type} implemented.")
-    torch.manual_seed(seed)
-    np.random.seed(seed)
-    random.seed(seed)
-
-    population_shape = (pop_size, dimensions)
-    init_pops = [
-        (env.upper_bound - env.lower_bound) * random_number_generator.rand(*population_shape) + env.lower_bound
-        for _ in range(num_starting_points)
-    ]
 
     aggregated_run_data = None
+    run_info = {
+        "agent_type": agent_type,
+        "environment": environment_type,
+        "seed": seed,
+        "num_runs": num_runs,
+        "num_batches": num_batches,
+    }
 
-    for i, init_pop in enumerate(init_pops):
-
-        if save_run_data:
-            run_data = pd.DataFrame({
-                'gen': [],
-                'action': [],
-                'reward': [],
-                'fbest': [],
-                'pop': [],
-                'states': [],
-                'es_run': []
-            })
-        for es_run in range(num_es_runs):
-            print(f"Starting {es_run + i * num_es_runs}/{len(init_pops)*num_es_runs}")
-            state = env.reset(init_pop)
+    try:
+        for run in range(num_runs):
+            if save_run_data:
+                actions = []
+                rewards = []
+                f_currs= []
+                states = []
+                batch_indeces = []
+                run_indeces = []
+            # TODO properly reset to different starting point
+            state = env.reset()
             agent.reset()
             if save_run_data:
-                run_data = run_data.append(
-                    {
-                        'gen': 0,
-                        'fbest': env.fbest,
-                        'pop': env.pop.tolist(),
-                        'states': state.tolist(),
-                        'es_run': es_run + i * num_es_runs
-                    },
-                    ignore_index=True)
-            for gen in range(1, num_gens):
-                if gen == 1:
-                    action = [0.5]
-                    if agent_type == 'random_agent':
-                        agent = RandomAgent(action_dim=1, seed=es_run+i*num_es_runs)
-                    elif "constant_agent" in agent_type:
-                        action = agent.select_action()
-                    elif agent_type == "step_agent":
-                        action = agent.select_action()
-                else:
-                    if agent_type == 'simple_cauchy_agent':
-                        action = (agent.calc_new_f_mu_simple_mean(
-                            env.mut, env.successes), )
-                    elif agent_type == 'lehmer_mean_agent':
-                        action = (agent.calc_new_f_mu(env.mut, env.successes),)
-                    elif agent_type == 'mde_cauchy_agent':
-                        action = (agent.calc_new_f_mu(env.mut, env.successes),)
-                    else:
-                        action = agent.select_action(state)
-                next_state, reward, done, cauchy_dict = env.step(action)
-                replay_buffer.add(state, action, next_state, reward, done)
+                actions.append(-1)
+                rewards.append(-1337)
+                f_currs.append(env.objective_function(env.x_cur))
+                states.append(state)
+                batch_indeces.append(0)
+                run_indeces.append(run)
+
+            for batch in range(1, num_batches):
+                print(f"Starting batch {batch}/{num_batches} of run {run}. \
+                    Total {batch + run * num_batches}/{num_runs * num_batches}")
+
+                action = agent.act(state)
+                next_state, reward, done, mutation_rate = env.step(action)
                 state = next_state
                 if save_run_data:
-                    run_data = run_data.append(
-                        {
-                            'gen': gen,
-                            'action': action[0],
-                            'reward': reward,
-                            'fbest': env.fbest,
-                            'pop': env.pop.tolist(),
-                            'states': state.tolist(),
-                            'es_run': es_run + i * num_es_runs
-                        },
-                        ignore_index=True)
-    
-        if save_run_data:
-            if aggregated_run_data is None:
-                aggregated_run_data = run_data
-            else:
-                aggregated_run_data = aggregated_run_data.append(
-                run_data, ignore_index=True)
-    now = datetime.now().strftime("%d-%m-%y_%H-%M")
-    # save_path = os.path.join(results_dir, f"rep_buffer_{now}")
-    save_path = os.path.join(results_dir, f"rep_buffer")
-    if save_rep_buffer:
-        replay_buffer.save(save_path)
-    run_info = {
-        'agent_type': agent_type,
-        'function_name': function_name,
-        'seed': seed,
-        'reward_structure': reward_structure,
-        'state_version': state_version,
-        'dimension': dimensions,
-        'num_gens': num_gens,
-        'num_es_runs': num_es_runs,
-        'pop_size': pop_size,
-        'init_pops': [pop.tolist() for pop in init_pops]
-    }
-    if save_rep_buffer:
-        with open(os.path.join(results_dir, f"run_info.json"), 'w') as f:
-            json.dump(run_info, f, indent=4)
+                    actions.append(action)
+                    rewards.append(reward)
+                    f_currs.append(env.objective_function(env.x_cur))
+                    states.append(state)
+                    batch_indeces.append(batch)
+                    run_indeces.append(run)
 
-    if not os.path.exists(results_dir):
-        os.makedirs(results_dir)
-    if save_run_data:
-        aggregated_run_data.to_csv(os.path.join(results_dir, "aggregated_run_data.csv"))
+            if save_run_data:
+                run_data = pd.DataFrame({
+                    "action": actions,
+                    "reward": rewards,
+                    "f_curr": f_currs,
+                    "state": states,
+                    "batch": batch_indeces,
+                    "run": run_indeces,
+                })
+                if aggregated_run_data is None:
+                    aggregated_run_data = run_data
+                else:
+                    aggregated_run_data = aggregated_run_data.append(
+                        run_data, ignore_index=True
+                    )
+    except OutOfTimeException:
+        save_data(
+            save_run_data,
+            aggregated_run_data,
+            save_rep_buffer,
+            replay_buffer,
+            results_dir,
+            run_info,
+        )
+        print("Saved checkpoint, because run was about to end")
+
+    save_data(
+        save_run_data,
+        aggregated_run_data,
+        save_rep_buffer,
+        replay_buffer,
+        results_dir,
+        run_info,
+    )
 
     if save_rep_buffer or save_run_data:
         msg = "Saved "
