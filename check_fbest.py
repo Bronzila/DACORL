83fc4774e180b9b52fc642bf330a8d2fbbd8fae7
Bronzila <janis_fix@web.de>
2024-03-25 14:54:40 +0100

    Adjust check_fbest script for improved result checking

--- check_fbest.py
+++ check_fbest.py
@@ -1,9 +1,12 @@
 import argparse
 import os
 import re
+import glob
+from pathlib import Path
 
 import numpy as np
 import pandas as pd
+
 
 
 def find_lowest_values(df, column_name, n=10):
@@ -50,16 +53,60 @@ if __name__ == "__main__":
         help="Get mean and std deviation of final solutions (default: True)",
         action=argparse.BooleanOptionalAction,
     )
+    parser.add_argument(
+        "--results",
+        help="Find results subfolders",
+        action=argparse.BooleanOptionalAction,
+    )
+    parser.add_argument(
+        "--verbose",
+        help="Verbose output",
+        action=argparse.BooleanOptionalAction,
+    )
     args = parser.parse_args()
 
+    paths = []
+    if args.results:
+        for folder_path, _, _ in os.walk(args.path):
+            paths.extend(glob.glob(os.path.join(folder_path, "*/eval_data.csv")))
+    else:
+        paths.append(args.path)
     # Load data
-    df = pd.read_csv(args.path)
-    print(f"Calculating for path {args.path}")
+    min_mean = np.inf
+    min_std = np.inf
+    min_path = ""
+    lowest_vals_of_min_mean = []
+    for path in paths:
+        incumbent_changed = False
+        df = pd.read_csv(path)
+        if args.verbose:
+            print(f"Calculating for path {path}")
 
-    if args.mean:
-        mean, std = calc_mean_and_std_dev(df)
-        print("Mean +- Std {mean:.3e} ± {std:.3e}".format(mean=mean, std=std))
-    if args.lowest:
-        lowest_vals = find_lowest_values(df, "f_cur", args.n)
-        print("Lowest values:")
-        print(lowest_vals[args.column_name])
+        if args.mean:
+            mean, std = calc_mean_and_std_dev(df)
+            mean = float(f"{mean:.3e}")
+            std = float(f"{std:.3e}")
+            if mean < min_mean:
+                incumbent_changed = True
+                min_mean = mean
+                min_std = std
+                min_path = path
+            elif mean == min_mean and std < min_std:
+                incumbent_changed = True
+                min_mean = mean
+                min_std = std
+                min_path = path
+            if args.verbose:
+                print("Mean +- Std {mean:.3e} ± {std:.3e}".format(mean=mean, std=std))
+        if args.lowest:
+            lowest_vals = find_lowest_values(df, "f_cur", args.n)
+            if incumbent_changed:
+                lowest_vals_of_min_mean = lowest_vals[args.column_name]
+            if args.verbose:
+                print("Lowest values:")
+                print(lowest_vals[args.column_name])
+    print("Overall lowest statistics:")
+    print(f"Found for path {min_path}")
+    print("Mean +- Std {mean:.3e} ± {std:.3e}".format(mean=min_mean, std=min_std))
+    print("Lowest values:")
+    print(lowest_vals_of_min_mean)
--- check_fbest_single.sh
+++ check_fbest_single.sh
@@ -1,10 +1,11 @@
-AGENT=constant
-FUNCTION=Ackley
-
 source activate MTORL-DAC
+id=combined
+root_dir=data_multi
 
-python3.10 check_fbest.py --path data/ToySGD/$AGENT/0/$FUNCTION//results/td3_bc/2000/eval_data.csv --lowest --mean
-python3.10 check_fbest.py --path data/ToySGD/$AGENT/0/$FUNCTION//results/td3_bc/4000/eval_data.csv --lowest --mean
-python3.10 check_fbest.py --path data/ToySGD/$AGENT/0/$FUNCTION//results/td3_bc/6000/eval_data.csv --lowest --mean
-python3.10 check_fbest.py --path data/ToySGD/$AGENT/0/$FUNCTION//results/td3_bc/8000/eval_data.csv --lowest --mean
-python3.10 check_fbest.py --path data/ToySGD/$AGENT/0/$FUNCTION//results/td3_bc/10000/eval_data.csv --lowest --mean
+for agent in exponential_decay #step_decay sgdr constant
+do
+    for function in Ackley Rastrigin Rosenbrock Sphere
+    do
+        python3.10 check_fbest.py --path $root_dir/ToySGD/$agent/$id/$function/results --results --mean --lowest
+    done
+done
