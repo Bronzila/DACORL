c4826cb75d307e53a91db49fb7cc984c68c49b67
Janis Fix <janis_fix@web.de>
2024-03-25 16:18:38 +0100

    Rewrite combine_buffers to also include combination of heterogeneous teachers

--- combine_buffers.py
+++ combine_buffers.py
@@ -2,28 +2,40 @@ import argparse
 import json
 from pathlib import Path
 
-from src.utils.general import combine_runs
+from src.utils.general import combine_runs, get_homogeneous_agent_paths
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Generate plots based on run data"
+        description="Combine generated data"
     )
     parser.add_argument(
         "--root_dir",
         help="Path where different agents are laying",
     )
     parser.add_argument(
+        "--custom_paths",
+        help="Path to a json file containing all paths to combine",
+    )
+    parser.add_argument(
         "--function",
         help="function",
     )
+    parser.add_argument(
+         "--combined_dir",
+         default="data_combined/ToySGD/combined",
+    )
     args = parser.parse_args()
 
-    combined_buffer, combined_run_info, combined_run_data = combine_runs(args.root_dir, args.function)
-
-    buffer_path = Path(args.root_dir, "combined", args.function, "rep_buffer")
-    run_info_path = Path(args.root_dir, "combined", args.function, "run_info.json")
-    run_data_path = Path(args.root_dir, "combined", args.function, "aggregated_run_data.csv")
-
+    if args.root_dir:
+        paths = get_homogeneous_agent_paths(args.root_dir, args.function)
+    elif args.custom_paths:
+        with Path(args.custom_paths).open("r") as f:
+            paths = json.load(f)
+    Path(args.combined_dir).mkdir(parents=True, exist_ok=True)
+    combined_buffer, combined_run_info, combined_run_data = combine_runs(paths)
+    buffer_path = Path(args.combined_dir, "rep_buffer")
+    run_info_path = Path(args.combined_dir, "run_info.json")
+    run_data_path = Path(args.combined_dir, "aggregated_run_data.csv")
     combined_buffer.save(buffer_path)
     with run_info_path.open(mode="w") as f:
             json.dump(combined_run_info, f, indent=4)
