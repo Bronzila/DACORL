945fd443a73b96e0e9f788797bbb2fa65cb936cc
Bronzila <janis_fix@web.de>
2024-04-24 13:15:41 +0200

    Add multi-seed and iqm option to tables

--- generate_tables.py
+++ generate_tables.py
@@ -56,6 +56,11 @@ if __name__ == "__main__":
         nargs="*",
         default=[0],
     )
+    parser.add_argument(
+        "--multi_seed",
+        help="Calculate table for multi-seed",
+        action=argparse.BooleanOptionalAction,
+    )
     args = parser.parse_args()
 
     base_path = Path(args.path)
@@ -65,9 +70,11 @@ if __name__ == "__main__":
         header = [None]
         header.extend(args.functions)
         rows_mean = [header]
+        rows_iqm = [header]
         rows_lowest = [header]
         for agent in args.agents:
             row_mean = [agent]
+            row_iqm = [agent]
             row_lowest = [agent]
             for function in args.functions:
                 if args.results:
@@ -77,16 +84,18 @@ if __name__ == "__main__":
                 if args.custom_path:                    
                     # run_data_path = base_path / agent / function / "results" if args.results else base_path / agent / function / "aggregated_run_data.csv"
                     run_data_path = base_path / function / "results" if args.results else base_path / function / "aggregated_run_data.csv"
-                mean, std, lowest, min_path = calculate_statistics(path=run_data_path, results=args.results, verbose=args.verbose)
+                mean, std, lowest, iqm, iqm_std, min_path = calculate_statistics(path=run_data_path, results=args.results, verbose=args.verbose)
                 pattern = r"(\d+)"
                 train_steps = int(re.findall(pattern, str(min_path))[-1]) if args.results else 0
                 if args.mean:
                     row_mean.append(f"{mean:.3e} ± {std:.3e}, {train_steps}")
+                    row_iqm.append(f"{iqm:.3e} ± {iqm_std:.3e}, {train_steps}")
                 if args.lowest:
                     lowest = lowest.to_numpy()[0]
                     row_lowest.append(f"{lowest:.3e}")
             if args.mean:
                 rows_mean.append(row_mean)
+                rows_iqm.append(row_iqm)
             if args.lowest:
                 rows_lowest.append(row_lowest)
 
@@ -96,6 +105,12 @@ if __name__ == "__main__":
         if args.mean:
             table_result_path = table_dir / f"mean_{agent_or_teacher}_{agent_id}.md"
             table = table_from_string_list(rows_mean)
+            markdown = generate_markdown(table)
+            with table_result_path.open("w") as f:
+                f.write(markdown)
+
+            table_result_path = table_dir / f"iqm_{agent_or_teacher}_{agent_id}.md"
+            table = table_from_string_list(rows_iqm)
             markdown = generate_markdown(table)
             with table_result_path.open("w") as f:
                 f.write(markdown)
