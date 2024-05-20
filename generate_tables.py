c946758d12ae362d595200b88525befbc768a3ee
gierle <leon.gieringer@gmx.de>
2024-05-20 17:28:45 +0200

    add iqm to table generator

--- generate_tables.py
+++ generate_tables.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
-from check_fbest import calculate_statistics
+from src.utils.general import calculate_multi_seed_statistics, calculate_single_seed_statistics
 from markdown_table_generator import generate_markdown, table_from_string_list
 
 if __name__ == "__main__":
@@ -66,42 +66,53 @@ if __name__ == "__main__":
             header = [None]
             header.extend(args.teacher)
             rows_mean = [header]
+            rows_iqm = [header]
             rows_lowest = [header]
             for i, agent in enumerate(args.agents):
                 row_mean = [agent]
+                row_iqm = [agent]
                 row_lowest = [agent]
                 for j, teacher in enumerate(args.teacher):
                     if agent == "teacher":
-                        run_data_paths = [base_path / teacher/ str(agent_id) / function / "aggregated_run_data.csv"]
+                        path = base_path / teacher/ str(agent_id) / function / "aggregated_run_data.csv"
+                        mean, std, lowest, iqm, iqm_std, min_path = calculate_single_seed_statistics(path=path, results=False, verbose=args.verbose)
                     else:
-                        for path in (base_path / teacher / str(agent_id) / function / "results" / agent).rglob(f"*/{args.hpo_budget}/eval_data.csv"):
-                            run_data_paths.append(path)
-                            break
-                        if len(run_data_paths) == 0:
-                            raise ValueError(f"No file found matching the pattern: \"*/{args.hpo_budget}/eval_data.csv\"")
-                    mean, std, lowest, lowest_std, _ = calculate_statistics(path=run_data_paths, results=False, verbose=args.verbose)
+                        path = base_path / teacher / str(agent_id) / function / "results" / agent
+                        mean, std, lowest, iqm, iqm_std, min_path = calculate_multi_seed_statistics(path=path, results=True, verbose=args.verbose)
 
                     if args.mean:
                         if mean is None:
                             row_mean.append(" ")
                             continue
                         row_mean.append(f"{mean:.3e} ± {std:.3e}")
+                        if iqm is None:
+                            row_iqm.append(" ")
+                            continue
+                        row_iqm.append(f"{iqm:.3e} ± {iqm_std:.3e}")
                     if args.lowest:
                         if lowest is None:
                             row_lowest.append(" ")
                             continue
                         lowest = lowest
-                        row_lowest.append(f"{lowest:.3e} ± {lowest_std:.3e}")
+                        row_lowest.append(f"{lowest.to_numpy()[0]:.3e}")
                 if args.mean:
                     rows_mean.append(row_mean)
+                    rows_iqm.append(row_iqm)
                 if args.lowest:
                     rows_lowest.append(row_lowest)
 
             table_dir = base_path / "tables"
             table_dir.mkdir(exist_ok=True)
             if args.mean:
+                # Regular mean
                 table_result_path = table_dir / f"mean_{function}_{agent_id}.md"
                 table = table_from_string_list(rows_mean)
+                markdown = generate_markdown(table)
+                with table_result_path.open("w") as f:
+                    f.write(markdown)
+                # IQM
+                table_result_path = table_dir / f"iqm_{function}_{agent_id}.md"
+                table = table_from_string_list(rows_iqm)
                 markdown = generate_markdown(table)
                 with table_result_path.open("w") as f:
                     f.write(markdown)
