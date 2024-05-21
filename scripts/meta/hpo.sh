a24818f8f316b1c104f02918da2e8fb81f9137a4
gierle <leon.gieringer@gmx.de>
2024-05-21 08:53:27 +0200

    adapt HPO script to allow for homogeneous training

--- scripts/meta/hpo.sh
+++ scripts/meta/hpo.sh
@@ -7,7 +7,7 @@
 #SBATCH -a 1-4 # array size
 #SBATCH --mem 8GB
 
-AGENT=${1:-exponential_decay}
+TEACHER=${1:-exponential_decay}
 RESULTS_DIR=${2:-data/data_hpo_10}
 RL_AGENT=${3:-td3_bc}
 ID=${4:-0}
@@ -24,23 +24,24 @@ source ~/.bashrc
 cd /work/dlclarge2/gieringl-DACORL/MTORL-DAC
 conda activate DACORL
 
+echo "Running in directory ($RESULTS_DIR) using the data teacher ($TEACHER) on ($ID) and training ($RL_AGENT)"
 
 if [ ${SLURM_ARRAY_TASK_ID} -eq 1 ]
 then
-    python data_gen.py --save_run_data --save_rep_buffer --env $FC1\_$VERSION --agent $AGENT --num_runs $NUM_RUNS --id $ID --results_dir $RESULTS_DIR
-    python train_hpo.py --data_dir $RESULTS_DIR/ToySGD/$AGENT/$ID/$FC1 --agent_type $RL_AGENT --output_path $RESULTS_DIR/smac/smac_$AGENT\_$FC1\_$RL_AGENT
+    # python data_gen.py --save_run_data --save_rep_buffer --env $FC1\_$VERSION --agent $TEACHER --num_runs $NUM_RUNS --id $ID --results_dir $RESULTS_DIR
+    python train_hpo.py --data_dir $RESULTS_DIR/ToySGD/$TEACHER/$ID/$FC1 --agent_type $RL_AGENT --output_path $RESULTS_DIR/smac/smac_$TEACHER\_$FC1\_$RL_AGENT --no-tanh_scaling
 elif [ ${SLURM_ARRAY_TASK_ID} -eq 2 ]
 then
-    python data_gen.py --save_run_data --save_rep_buffer --env $FC2\_$VERSION --agent $AGENT --num_runs $NUM_RUNS --id $ID --results_dir $RESULTS_DIR
-    python train_hpo.py --data_dir $RESULTS_DIR/ToySGD/$AGENT/$ID/$FC2 --agent_type $RL_AGENT --output_path $RESULTS_DIR/smac/smac_$AGENT\_$FC2\_$RL_AGENT
+    # python data_gen.py --save_run_data --save_rep_buffer --env $FC2\_$VERSION --agent $TEACHER --num_runs $NUM_RUNS --id $ID --results_dir $RESULTS_DIR
+    python train_hpo.py --data_dir $RESULTS_DIR/ToySGD/$TEACHER/$ID/$FC2 --agent_type $RL_AGENT --output_path $RESULTS_DIR/smac/smac_$TEACHER\_$FC2\_$RL_AGENT --no-tanh_scaling
 elif [ ${SLURM_ARRAY_TASK_ID} -eq 3 ]
 then
-    python data_gen.py --save_run_data --save_rep_buffer --env $FC3\_$VERSION --agent $AGENT --num_runs $NUM_RUNS --id $ID --results_dir $RESULTS_DIR
-    python train_hpo.py --data_dir $RESULTS_DIR/ToySGD/$AGENT/$ID/$FC3 --agent_type $RL_AGENT --output_path $RESULTS_DIR/smac/smac_$AGENT\_$FC3\_$RL_AGENT
+    # python data_gen.py --save_run_data --save_rep_buffer --env $FC3\_$VERSION --agent $TEACHER --num_runs $NUM_RUNS --id $ID --results_dir $RESULTS_DIR
+    python train_hpo.py --data_dir $RESULTS_DIR/ToySGD/$TEACHER/$ID/$FC3 --agent_type $RL_AGENT --output_path $RESULTS_DIR/smac/smac_$TEACHER\_$FC3\_$RL_AGENT --no-tanh_scaling
 elif [ ${SLURM_ARRAY_TASK_ID} -eq 4 ]
 then
-    python data_gen.py --save_run_data --save_rep_buffer --env $FC4\_$VERSION --agent $AGENT --num_runs $NUM_RUNS --id $ID --results_dir $RESULTS_DIR
-    python train_hpo.py --data_dir $RESULTS_DIR/ToySGD/$AGENT/$ID/$FC4 --agent_type $RL_AGENT --output_path $RESULTS_DIR/smac/smac_$AGENT\_$FC4\_$RL_AGENT
+    # python data_gen.py --save_run_data --save_rep_buffer --env $FC4\_$VERSION --agent $TEACHER --num_runs $NUM_RUNS --id $ID --results_dir $RESULTS_DIR
+    python train_hpo.py --data_dir $RESULTS_DIR/ToySGD/$TEACHER/$ID/$FC4 --agent_type $RL_AGENT --output_path $RESULTS_DIR/smac/smac_$TEACHER\_$FC4\_$RL_AGENT --no-tanh_scaling
 fi
 
 # Print some Information about the end-time to STDOUT
--- scripts/meta/run_hpo.sh
+++ scripts/meta/run_hpo.sh
@@ -1,10 +1,11 @@
 RESULTS_DIR=${1:-data}
+ID=${2:-0}
 
 strings=("bc" "td3_bc" "cql" "awac" "edac" "sac_n" "lb_sac" "iql")
 for i in "${strings[@]}"
 do
-	sbatch --bosch scripts/meta/hpo.sh exponential_decay $i $RESULTS_DIR
-	sbatch --bosch scripts/meta/hpo.sh step_decay $i $RESULTS_DIR
-	sbatch --bosch scripts/meta/hpo.sh sgdr $i $RESULTS_DIR
-	sbatch --bosch scripts/meta/hpo.sh constant $i $RESULTS_DIR
+	sbatch --bosch scripts/meta/hpo.sh exponential_decay $RESULTS_DIR $i $ID
+	sbatch --bosch scripts/meta/hpo.sh step_decay $RESULTS_DIR $i $ID
+	sbatch --bosch scripts/meta/hpo.sh sgdr $RESULTS_DIR $i $ID
+	sbatch --bosch scripts/meta/hpo.sh constant $RESULTS_DIR $i $ID
 done
