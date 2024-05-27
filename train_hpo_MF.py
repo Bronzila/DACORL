41158f4118a69cd15f182293fdc03e45ce07618a
gierle <leon.gieringer@gmx.de>
2024-05-27 14:32:44 +0200

    add MF-HPO (SAF) training

--- train_hpo_MF.py
+++ train_hpo_MF.py
@@ -4,18 +4,16 @@ import warnings
 from pathlib import Path
 
 import numpy as np
-import torch.nn as nn
 from ConfigSpace import (
     Categorical,
     Configuration,
     ConfigurationSpace,
+    Constant,
     Float,
     Integer,
-    Constant,
 )
 from matplotlib import pyplot as plt
 from smac import (
-    HyperbandFacade,
     MultiFidelityFacade as MFFacade,
     Scenario,
 )
@@ -32,10 +30,21 @@ class Optimizee:
         data_dir: str,
         agent_type: str,
         debug: bool,
+        budget: int,
+        eval_protocol: str,
+        eval_seed: int,
+        seed: int,
+        tanh_scaling: bool,
     ) -> None:
         self.data_dir = data_dir
         self.agent_type = agent_type
         self.debug = debug
+        self.eval_protocol = eval_protocol
+        self.eval_seed = eval_seed
+        self.budget = budget
+        self.rng = np.random.default_rng(seed)
+        self.seeds = self.rng.integers(0, 2**32 - 1, size=12)
+        self.tanh_scaling = tanh_scaling
 
         with Path(self.data_dir, "run_info.json").open(mode="rb") as f:
             self.run_info = json.load(f)
@@ -50,46 +59,14 @@ class Optimizee:
         hidden_layers_critic = Integer(
             "hidden_layers_critic", (0, 5), default=1
         )
-        hidden_dim = Categorical("hidden_dim", [32, 64, 128, 256, 512], default=256)
-        activation = Categorical(
-            "activation", ["ReLU", "LeakyReLU"], default="ReLU"
-        )
-        batch_size = Categorical(
-            "batch_size", [2, 4, 8, 16, 32, 64, 128, 256], default=64
-        )
-        discount_factor = Float("discount_factor", (0, 1), default=0.99)
-        target_update_rate = Float("target_update_rate", (0, 1), default=5e-3)
-        # Add the parameters to configuration space
-        cs.add_hyperparameters(
-            [
-                lr_actor,
-                lr_critic,
-                hidden_layers_actor,
-                hidden_layers_critic,
-                hidden_dim,
-                activation,
-                batch_size,
-                discount_factor,
-                target_update_rate,
-            ]
-        )
-        return cs
-
-    @property
-    def configspace_reduced(self) -> ConfigurationSpace:
-        cs = ConfigurationSpace()
-
-        lr_actor = Float("lr_actor", (1e-5, 1e-2), default=3e-4)
-        lr_critic = Float("lr_critic", (1e-5, 1e-2), default=3e-4)
-        hidden_layers_actor = Constant("hidden_layers_actor", 1)
-        hidden_layers_critic = Constant("hidden_layers_critic", 1)
-        hidden_dim = Constant("hidden_dim", 256)
+        actor_hidden_dim = Categorical("actor_hidden_dim", [2, 4, 8, 16, 32, 64, 128, 256], default=64)
+        critic_hidden_dim = Categorical("critic_hidden_dim", [2, 4, 8, 16, 32, 64, 128, 256], default=64)
         activation = Constant("activation", "ReLU")
         batch_size = Categorical(
             "batch_size", [2, 4, 8, 16, 32, 64, 128, 256], default=64
         )
-        discount_factor = Float("discount_factor", (0, 1), default=0.99)
-        target_update_rate = Float("target_update_rate", (0, 1), default=5e-3)
+        discount_factor = Categorical("discount_factor", [0.9, 0.99, 0.999, 0.9999], default=0.99)
+        target_update_rate = Float("target_update_rate", (0, 0.25), default=5e-3)
         # Add the parameters to configuration space
         cs.add_hyperparameters(
             [
@@ -97,11 +74,12 @@ class Optimizee:
                 lr_critic,
                 hidden_layers_actor,
                 hidden_layers_critic,
-                hidden_dim,
+                actor_hidden_dim,
+                critic_hidden_dim,
                 activation,
                 batch_size,
-                # discount_factor,
-                # target_update_rate,
+                discount_factor,
+                target_update_rate,
             ],
         )
         return cs
@@ -109,26 +87,33 @@ class Optimizee:
     def train(
         self, config: Configuration, seed: int = 0, budget: int = 25
     ) -> float:
-        log_dict, eval_mean = train_agent(
-            data_dir=self.data_dir,
-            agent_type=self.agent_type,
-            agent_config={},
-            num_train_iter=budget,
-            num_eval_runs=100,
-            batch_size=config["batch_size"],
-            val_freq=int(budget),
-            seed=seed,
-            wandb_group=None,
-            timeout=0,
-            hyperparameters=config,
-            debug=self.debug,
-            use_wandb=False,
-        )
+        results = []
+        for _seed in self.seeds[:int(round(budget))]:
+            log_dict, eval_mean = train_agent(
+                data_dir=self.data_dir,
+                agent_type=self.agent_type,
+                agent_config={},
+                num_train_iter=self.budget,
+                batch_size=config["batch_size"],
+                val_freq=int(self.budget),
+                seed=_seed,
+                wandb_group=None,
+                timeout=0,
+                hyperparameters=config,
+                debug=self.debug,
+                tanh=self.tanh_scaling,
+                eval_protocol=self.eval_protocol,
+                eval_seed=self.eval_seed,
+            )
+            print(f"Mean for seed {_seed}: {eval_mean}")
+            results.append(eval_mean)
 
-        return eval_mean
+        seed_aggregated_mean = np.array(results).mean()
+        print(f"aggregated mean: {seed_aggregated_mean}")
+        return seed_aggregated_mean
 
 
-def plot_trajectory(facade: MFFacade) -> None:
+def plot_trajectory(facade: MFFacade, output_path) -> None:
     """Plots the trajectory (incumbents) of the optimization process."""
     plt.figure()
     plt.title("Trajectory")
@@ -153,7 +138,7 @@ def plot_trajectory(facade: MFFacade) -> None:
     plt.scatter(X, Y, marker="x")
 
     plt.legend()
-    plt.show()
+    plt.savefig(output_path / "traj.svg")
 
 
 if __name__ == "__main__":
@@ -165,24 +150,30 @@ if __name__ == "__main__":
         help="path to the directory where replay_buffer and info about the replay_buffer are stored",
     )
     parser.add_argument(
-        "--agent_type", type=str, default="td3_bc", choices=["bc", "td3_bc", "cql", "awac", "edac", "sac_n", "lb_sac"]
+        "--agent_type", type=str, default="td3_bc", choices=["bc", "td3_bc", "cql", "awac", "edac", "sac_n", "lb_sac", "iql", "dt"]
     )
     parser.add_argument("--seed", type=int, default=0)
-    parser.add_argument(
-        "--reduced",
-        action="store_true",
-        help="If set, architectural parameters will be constant",
-    )
-    parser.add_argument(
-        "--save_incumbent",
-        action="store_true",
-        default=True,
-        help="Flag if we save the incumbent configuration"
-    )
+    parser.add_argument("--time_limit", type=int, default=30)
+    parser.add_argument("--budget", type=int, default=15000)
     parser.add_argument(
         "--debug",
         action="store_true",
         help="Run for max. 5 iterations and don't log in wanbd.",
+    )
+    parser.add_argument(
+        "--output_path",
+        type=str,
+        help="Path where optimization logs are saved",
+        default="smac"
+    )
+    parser.add_argument(
+        "--eval_protocol", type=str, default="train", choices=["train", "interpolation"]
+    )
+    parser.add_argument("--eval_seed", type=int, default=123)
+    parser.add_argument(
+        "--tanh_scaling",
+        action=argparse.BooleanOptionalAction,
+        default=True,
     )
 
     args = parser.parse_args()
@@ -192,22 +183,29 @@ if __name__ == "__main__":
         data_dir=args.data_dir,
         agent_type=args.agent_type,
         debug=args.debug,
+        budget=args.budget,
+        eval_protocol=args.eval_protocol,
+        eval_seed=args.eval_seed,
+        seed=args.seed,
+        tanh_scaling=args.tanh_scaling,
     )
-
+    output_path = Path(args.output_path)
     scenario = Scenario(
-        (
-            optimizee.configspace_reduced
-            if args.reduced
-            else optimizee.configspace
-        ),
-        n_trials=250,  # Evaluate max 500 different trials
-        min_budget=1000,  # Train the MLP using a hyperparameter configuration for at least 5 epochs
-        max_budget=10000,  # Train the MLP using a hyperparameter configuration for at most 25 epochs
+        optimizee.configspace,
+        output_directory=output_path,
+        walltime_limit=60 * 60 * args.time_limit,  # convert hours into seconds
+        n_trials=500,
+        min_budget=3,
+        max_budget=12,
         n_workers=1,
+        deterministic=True,
     )
 
     # We want to run five random configurations before starting the optimization.
     initial_design = MFFacade.get_initial_design(scenario, n_configs=5)
+
+    # Use eta=2 to get brackets with [3, 6, 12] seeds
+    intensifier = MFFacade.get_intensifier(scenario, eta=2, incumbent_selection="highest_budget")
 
     # Create our SMAC object and pass the scenario and the train method
     smac = MFFacade(
@@ -215,16 +213,20 @@ if __name__ == "__main__":
         optimizee.train,
         initial_design=initial_design,
         overwrite=True,
+        intensifier=intensifier,
         logging_level=20,
     )
     incumbent = smac.optimize()
 
-    if args.save_incumbent:
-        save_config_dir = Path(args.data_dir) / "results" / args.agent_type
-        save_config_dir.mkdir(exist_ok=True)
-        path = save_config_dir / "incumbent.json"
-        print(f"Saving incumbent to : {path}")
-        with path.open("w") as f:
-            json.dump(incumbent.get_dictionary(), f, indent=4)
+    print("Incumbent:")
+    print(incumbent)
+    print(f"Final score: {smac.validate(incumbent)}")
 
-    print(smac.validate(incumbent))
+    plot_trajectory(smac, output_path)
+
+    save_config_dir = Path(args.data_dir) / "results" / args.agent_type
+    save_config_dir.mkdir(exist_ok=True)
+    path = save_config_dir / "incumbent.json"
+    print(f"Saving incumbent to : {path}")
+    with path.open("w") as f:
+        json.dump(incumbent.get_dictionary(), f, indent=4)
