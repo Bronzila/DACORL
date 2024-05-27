7ea9b6891f6ab61897f65dd1b6b7f5d1f28a15b4
gierle <leon.gieringer@gmx.de>
2024-05-27 14:30:06 +0200

    add generate_tables script for meta

---
+++ scripts/meta/run_generate_tables.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH -p bosch_cpu-cascadelake # partition (queue)
+#SBATCH -t 0-00:10:00
+#SBATCH -o logs/%A.%N.o       # STDOUT  (the folder log has to exist) %A will be replaced by the SLURM_ARRAY_JOB_ID value
+#SBATCH -e logs/%A.%N.e       # STDERR  (the folder log has to exist) %A will be replaced by the SLURM_ARRAY_JOB_ID value
+#SBATCH -J Statistics # sets the job name. 
+#SBATCH --mem 8GB
+
+RESULTS_DIR=${1:-data}
+ID=${2:-0}
+FIDELITY=${3:-10000}
+
+start=`date +%s`
+
+source ~/.bashrc
+cd /work/dlclarge2/gieringl-DACORL/MTORL-DAC
+conda activate DACORL
+
+python -W ignore generate_tables.py --path $RESULTS_DIR/ToySGD/  --mean --lowest --ids $ID --hpo_budget $FIDELITY
+python -W ignore generate_results_markdown.py $RESULTS_DIR $ID
+
+
+# Print some Information about the end-time to STDOUT
+end=`date +%s`
+runtime=$((end-start))
+
+echo "DONE";
+echo "Finished at $(date)";
