5c1db845456ce30b84da0988a673852e3b29ff08
gierle <leon.gieringer@gmx.de>
2024-05-27 14:29:07 +0200

    add MF-HPO script for meta

---
+++ scripts/meta/run_mf_hpo.sh
@@ -0,0 +1,11 @@
+RESULTS_DIR=${1:-data}
+ID=${2:-0}
+
+strings=("bc" "td3_bc" "cql" "awac" "edac" "sac_n" "lb_sac" "iql")
+for i in "${strings[@]}"
+do
+	sbatch --bosch scripts/meta/mf_hpo.sh exponential_decay $RESULTS_DIR $i $ID
+	sbatch --bosch scripts/meta/mf_hpo.sh step_decay $RESULTS_DIR $i $ID
+	sbatch --bosch scripts/meta/mf_hpo.sh sgdr $RESULTS_DIR $i $ID
+	sbatch --bosch scripts/meta/mf_hpo.sh constant $RESULTS_DIR $i $ID
+done
