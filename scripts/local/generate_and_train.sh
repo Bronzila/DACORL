f7c3c26adbc9f857cb773acec12e7a1cd4ebeb6d
gierle <leon.gieringer@gmx.de>
2024-07-05 13:58:01 +0200

    fix local data_gen and train script

--- scripts/local/generate_and_train.sh
+++ scripts/local/generate_and_train.sh
@@ -1,18 +1,18 @@
 source activate DACORL
 id=0
 root_dir=data/new_debug_agents
-fidelity=1000
-num_runs=100
+fidelity=30000
+num_runs=1000
 state_version=extended_velocity
 
 for teacher in exponential_decay step_decay sgdr constant
 do
     for function in Ackley Rastrigin Rosenbrock Sphere
     do
-        for agent in bc td3_bc cql awac edac lb_sac sac_n
+        for agent in bc td3_bc cql awac edac lb_sac sac_n td3
         do
-            python -W ignore data_gen.py --results_dir $root_dir --env $function\_$state_version --agent $teacher --num_runs $num_runs  --save_run_data --save_rep_buffer
-            python -W ignore train.py --data_dir $root_dir/ToySGD/$teacher/$id/$function/ --agent_type $agent --num_train_iter $fidelity --num_eval_runs $num_runs --val_freq $fidelity  --no-use_wandb
+            python -W ignore data_gen.py --results_dir $root_dir --env $function\_$state_version --agent $teacher --num_runs $num_runs  --save_run_data --save_rep_buffer --benchmark ToySGD
+            python -W ignore train.py --data_dir $root_dir/ToySGD/$teacher/$id/$function/ --agent_type $agent --num_train_iter $fidelity --num_eval_runs $num_runs --val_freq $fidelity  --no-wandb
         done
     done
 done
