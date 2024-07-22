79fa06e315ac3944ce5b4c71c52ce93f16b116a4
Janis Fix <janis_fix@web.de>
2024-07-22 16:58:11 +0200

    [WIP] Add training and evaluation scripts for SGD

--- src/utils/test_agent.py
+++ src/utils/test_agent.py
@@ -105,3 +105,81 @@ def test_agent(
             "batch": batches,
         },
     )
+
+def test_agent_SGD(
+    actor: Any,
+    env: Any,
+    n_runs: int,
+    n_batches: int,
+    seed: int,
+) -> pd.DataFrame:
+    env.seed(seed)
+    set_seeds(seed)
+    actor.eval()
+
+    actions = []
+    rewards = []
+    train_loss = []
+    train_acc = []
+    valid_loss = []
+    valid_acc = []
+    test_loss = []
+    test_acc = []
+    states = []
+    runs = []
+    batches = []
+
+    for run in range(n_runs):
+        state, meta_info = env.reset()
+
+        # Save start of optimization
+        rewards.append(np.NaN)
+        states.append(state.numpy())
+        batches.append(0)
+        runs.append(run)
+        actions.append(math.log10(env.learning_rate))
+        train_loss.append(env.train_loss)
+        valid_loss.append(env.validation_loss)
+        train_acc.append(env.train_accuracy)
+        valid_acc.append(env.validation_accuracy)
+        test_loss.append(env.test_loss)
+        test_acc.append(env.test_accuracy)
+
+        for batch in range(1, n_batches + 1): # As we start with batch 1 and not 0, add 1
+
+            action = actor.act(state)
+            next_state, reward, done, truncated, info = env.step(action)
+
+            actions.append(action)
+            rewards.append(reward.numpy())
+            states.append(state.numpy())
+            batches.append(batch)
+            runs.append(run)
+            train_loss.append(env.train_loss)
+            valid_loss.append(env.validation_loss)
+            train_acc.append(env.train_accuracy)
+            valid_acc.append(env.validation_accuracy)
+            test_loss.append(env.test_loss)
+            test_acc.append(env.test_accuracy)
+
+            state = next_state
+            if done:
+                break
+
+
+    actor.train()
+    return pd.DataFrame(
+        {
+            "action": actions,
+            "reward": rewards,
+            "train_loss": train_loss,
+            "valid_loss": valid_loss,
+            "train_acc": train_acc,
+            "valid_acc": valid_acc,
+            "test_loss": test_loss,
+            "test_acc": test_acc,
+            "states": states,
+            "run": runs,
+            "batch": batches,
+        },
+    )
--- src/utils/train_agent.py
+++ src/utils/train_agent.py
@@ -8,6 +8,7 @@ import torch
 import wandb
 
 from src.utils.general import (
+    calc_mean_and_std_dev,
     get_agent,
     get_environment,
     save_agent,
@@ -15,7 +16,7 @@ from src.utils.general import (
     set_timeout,
 )
 from src.utils.replay_buffer import ReplayBuffer
-from src.utils.test_agent import test_agent
+from src.utils.test_agent import test_agent, test_agent_SGD
 
 
 def train_agent(
@@ -66,7 +67,7 @@ def train_agent(
         )
 
     logs = {"actor_loss": [], "critic_loss": []}
-    min_fbest = np.inf
+    inc_value = None
     for t in range(int(num_train_iter)):
         batch = replay_buffer.sample(batch_size)
         log_dict = agent.train(batch)
@@ -79,24 +80,43 @@ def train_agent(
         if val_freq != 0 and (t + 1) % val_freq == 0:
             with torch.random.fork_rng():
                 env = get_environment(run_info["environment"])
-                eval_runs = num_eval_runs if num_eval_runs is not None else len(run_info["starting_points"])
-                if eval_protocol == "train":
-                    eval_data = test_agent(
-                        actor=agent.actor,
-                        env=env,
-                        n_runs=eval_runs,
-                        starting_points=run_info["starting_points"],
-                        n_batches=run_info["environment"]["num_batches"],
-                        seed=run_info["seed"],
-                    )
-                elif eval_protocol == "interpolation":
-                    eval_data = test_agent(
-                        actor=agent.actor,
-                        env=env,
-                        n_runs=eval_runs,
-                        n_batches=run_info["environment"]["num_batches"],
-                        seed=eval_seed,
-                    )
+                if run_info["environment"]["type"] == "ToySGD":
+                    eval_runs = num_eval_runs if num_eval_runs is not None else len(run_info["starting_points"])
+                    if eval_protocol == "train":
+                        eval_data = test_agent(
+                            actor=agent.actor,
+                            env=env,
+                            n_runs=eval_runs,
+                            starting_points=run_info["starting_points"],
+                            n_batches=run_info["environment"]["num_batches"],
+                            seed=run_info["seed"],
+                        )
+                    elif eval_protocol == "interpolation":
+                        eval_data = test_agent(
+                            actor=agent.actor,
+                            env=env,
+                            n_runs=eval_runs,
+                            n_batches=run_info["environment"]["num_batches"],
+                            seed=eval_seed,
+                        )
+                elif run_info["environment"]["type"] == "SGD":
+                    n_batches_total = run_info["environment"]["num_epochs"] * len(env.train_loader)
+                    if eval_protocol == "train":
+                        eval_data = test_agent_SGD(
+                            actor=agent.actor,
+                            env=env,
+                            n_runs=num_eval_runs,
+                            n_batches=n_batches_total,
+                            seed=run_info["seed"],
+                        )
+                    elif eval_protocol == "interpolation":
+                        eval_data = test_agent_SGD(
+                            actor=agent.actor,
+                            env=env,
+                            n_runs=num_eval_runs,
+                            n_batches=n_batches_total,
+                            seed=eval_seed,
+                        )
 
                 # Save agent early to enable continuation of pipeline
                 save_agent(agent.state_dict(), results_dir, t, seed)
@@ -105,15 +125,40 @@ def train_agent(
                     json.dump(dict(hyperparameters), f, indent=2)
 
                 # Calculate mean performance for this checkpoint
-                final_evaluations = eval_data.groupby("run").last()
-                fbests = final_evaluations["f_cur"]
-                fbest_mean = fbests.mean()
-                print(f"Mean at iteration {t+1}: {fbest_mean}")
-                min_fbest = np.min([min_fbest, fbest_mean])
+                if run_info["environment"]["type"] == "ToySGD":
+                    fbest_mean, _ = calc_mean_and_std_dev(eval_data)
+                    print(f"Mean at iteration {t+1}: {fbest_mean}")
+                    if inc_value is None:
+                        inc_value = fbest_mean
+                    else:
+                        inc_value = np.min([inc_value, fbest_mean])
+                elif run_info["environment"]["type"] == "SGD":
+                    # Statistics for train set
+                    train_loss_mean, train_loss_std = calc_mean_and_std_dev(eval_data, "train_loss")
+                    print(f"Mean train loss at iteration {t+1}: {train_loss_mean} +- {train_loss_std}")
+                    train_acc_mean, train_acc_std = calc_mean_and_std_dev(eval_data, "train_acc")
+                    print(f"Mean train acc at iteration {t+1}: {train_acc_mean} +- {train_acc_std}")
+
+                    # Statistics for validation set
+                    valid_loss_mean, valid_loss_std = calc_mean_and_std_dev(eval_data, "valid_loss")
+                    print(f"Mean valid loss at iteration {t+1}: {valid_loss_mean} +- {valid_loss_std}")
+                    valid_acc_mean, valid_acc_std = calc_mean_and_std_dev(eval_data, "valid_acc")
+                    print(f"Mean valid acc at iteration {t+1}: {valid_acc_mean} +- {valid_acc_std}")
+
+                    # Statistics for test set
+                    test_loss_mean, test_loss_std = calc_mean_and_std_dev(eval_data, "test_loss")
+                    print(f"Mean test loss at iteration {t+1}: {test_loss_mean} +- {test_loss_std}")
+                    test_acc_mean, test_acc_std = calc_mean_and_std_dev(eval_data, "test_acc")
+                    print(f"Mean test acc at iteration {t+1}: {test_acc_mean} +- {test_acc_std}")
+
+                    if inc_value is None:
+                        inc_value = test_acc_mean
+                    else:
+                        inc_value = np.max([inc_value, test_acc_mean])
 
     save_agent(agent.state_dict(), results_dir, t, seed)
 
     if not debug:
         wandb.finish()  # type: ignore
 
-    return logs, min_fbest
+    return logs, inc_value
