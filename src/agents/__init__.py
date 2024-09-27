ebdb86792d8f7008ff4a03a757f0e0e5a63dacd4
gierle <leon.gieringer@gmx.de>
2024-09-27 14:00:16 +0200

    remove unused TD3+BC

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -5,7 +5,6 @@ from src.agents.exponential_decay import ExponentialDecay
 from src.agents.sgdr import SGDR
 from src.agents.step_decay import StepDecay
 from src.agents.td3 import TD3
-from src.agents.td3_bc_agent import TD3_BC
 
 __all__ = [
     "ExponentialDecay",
@@ -13,7 +12,6 @@ __all__ = [
     "SGDR",
     "Constant",
     "ConstantCMAES",
-    "TD3_BC",
     "CSA",
     "TD3",
 ]
